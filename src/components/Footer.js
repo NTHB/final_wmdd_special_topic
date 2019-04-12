e0a9ba234a320588280c3370c7404d653fe43fe6
Noppawit <noppawit.han@gmail.com>
2019-04-11 17:28:28 -0700

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+const Footer= () => (
+    <div style={{textAlign: 'center', marginTop: '2rem', width: '100%', backgroundColor: 'white', padding: '1rem'}}>
+        <p style={{margin: 0}}>Food from <a href='https://yelp.com/'>YELP.com</a> , Partial design by <a href='https://material-ui.com/'>Material-UI</a></p>
+        <p>Noppawit Hansompob</p>
+    </div>
+)
+ 
+export default Footer
